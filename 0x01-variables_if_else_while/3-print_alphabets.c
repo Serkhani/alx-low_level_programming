f0a1fbd2fb99f973548d469b76b5fdefbda8caa1
Serkhani <deviper03@gmail.com>
2022-07-01 14:30:18 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+  char letter = 'a';
+  while (letter <= 'z')
+    {
+      putchar(letter);
+      letter++;
+    }
+  letter = 'A';
+  while (letter <= 'Z')
+    {
+      putchar(letter);
+      letter++;
+    }
+  putchar('\n');
+  return (0);
+}
