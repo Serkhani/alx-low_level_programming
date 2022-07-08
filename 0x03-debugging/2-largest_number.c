8ce71c245d365854dc004932d86b61b8267992ce
Serkhani <deviper03@gmail.com>
2022-07-07 19:26:18 -0700

    hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int i, int j, int k)
 {
 	int largest;
 
-	if(a >= b && a >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if(b >= a && b >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
