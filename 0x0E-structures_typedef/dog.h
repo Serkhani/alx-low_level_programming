18cff76192f2c23b32d9175c06b77fa5f3bc4f05
Serkhani <deviper03@gmail.com>
2022-08-01 13:11:22 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - a structure that contains dog
+ * @name: pointer to a string representing name of dog
+ * @age: float containing the age of dog
+ * @char: ptr to an array of characters
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
